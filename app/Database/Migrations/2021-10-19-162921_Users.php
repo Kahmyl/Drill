ab92bf6516e7110342a6c988b7fadbf620725f9f
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2021-10-19 19:05:53 +0100

    codeigniter

---
+++ app/Database/Migrations/2021-10-19-162921_Users.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'TEXT',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Views/auth/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in</title>
+    <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
+</head>
+<body>
+    <div class="container">
+        <div class="row" style="margin-top:45px;">
+            <div class="col-md-4 col-md-offset-4">
+                <h4>Sign In</h4><br>
+                <form action="">
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="text" class="form-control" name="password">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary btn-block" type="submit">Sign In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
